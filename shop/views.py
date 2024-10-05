db67e1813d97ec076eb1b578488d3fbb685b1a71
shaghayegh1383 <sh.s.m1383@gmail.com>
2024-10-05 18:30:18 +0330

    Initial commit for Django project

---
+++ shop/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render , HttpResponse
+from . models import Product
+
+def helloworld(request):
+    all_products = Product.objects.all()
+    
+    return render (request , 'index.html',{'products':all_products})
