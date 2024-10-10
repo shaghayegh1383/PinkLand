198ee2fc3fb77f7bed83a9fd91db0d3ce58de454
shaghayegh1383 <sh.s.m1383@gmail.com>
2024-10-10 22:50:23 +0330

    initial commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path , include
-from . views import helloworld
+from . views import helloworld , about , login_user , logout_user
 
 urlpatterns = [
-    path('', helloworld)
+    path('', helloworld , name= 'home'),
+    path ('about/',about , name= 'about'),
+    path('login', login_user , name= 'login'),
+    path('logout', logout_user , name= 'logout'),
+    
+    
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render , redirect
 from . models import Product
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
 
 def helloworld(request):
     all_products = Product.objects.all()
     
     return render (request , 'index.html',{'products':all_products})
+
+def about(request):
+    return render(request , 'about.html')
+
+def login_user(request):
+    return render(request,'login.html')
+   
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,'با موفقیت خارج شدید')
+    return redirect("home")
+
+
